438bc18bee0cf13d217c75985341075114261634
emily <emilyrpj11@gmail.com>
2024-07-12 02:18:44 +0200

    windchill check

--- scripts/place.js
+++ scripts/place.js
@@ -9,4 +9,26 @@ copyrightYear.textContent = currentYear;
 const lastModified = document.getElementById('last-modified');
 lastModified.textContent = document.lastModified;
 
-//------------------------------------------
+//-----------------------------------------------------------
+// The temperature and wind speed values (Degree Celsius & km/h)
+const temperature = 22;
+const windSpeed = 11; 
+
+// Calculate the wind chill factor & Verify if conditions are true
+function calculateWindChill(temp, windSpeed) {
+    if (temp <= 10 && windSpeed > 4.8) {
+
+        // Wind Chill formula
+        const windChill = 13.12 + (0.6215 * temp) - (11.37 * Math.pow(windSpeed, 0.16)) + (0.3965 * temp * Math.pow(windSpeed, 0.16));
+
+        return Math.round(windChill);
+    } else {
+        return 'N/A';
+    }
+}
+
+// Calculate and display the wind chill factor
+const windChill = calculateWindChill(temp, windSpeed);
+document.getElementById('wind-chill').textContent = windChill;
+
+// ----------------------------------------------------------
