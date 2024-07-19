ec28b6247a622b4a38fc84e19d2916be395fa821
emily <emilyrpj11@gmail.com>
2024-07-19 15:39:38 +0200

    Mobile check

--- scripts/filtered-temple.js
+++ scripts/filtered-temple.js
@@ -85,21 +85,60 @@ const temples = [
         templeName: "Accra Ghana",
         location: "Accra, Ghana",
         dedicated: "2003, January, 11",
-        area: "17500",
+        area: 17500,
         imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/accra-ghana/400x225/accra-ghana-temple-detail-249022-2400x1200.jpg",
     },
     {
         templeName: "Johannesburg South Africa",
         location: "Johannesburg, South Africa",
         dedicated: "1985, August, 24-25",
-        area: "19184",
-        imageUrl: "https://images.app.goo.gl/p7ek3LpgEuoun1F47https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/johannesburg-south-africa/400x250/johannesburg-south-africa-temple-lds-83166-wallpaper.jpg",
+        area: 19184,
+        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/johannesburg-south-africa/400x250/johannesburg-south-africa-temple-lds-83166-wallpaper.jpg",
     },
     {
-        templeName: "Salt Lake Temple",
+        templeName: "Salt Lake City",
         location: "Salt Lake City, Utah",
         dedicated: "1893, April, 6-24",
-        area: "382207",
+        area: 382207,
         imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/salt-lake-city-utah/2018/400x250/slctemple5.jpg",
     },
 ];
+
+createTempleCard(temples);
+
+const oldtempleLink = document.querySelector("#oldtemple");
+const newtempleLink = document.querySelector("#newtemple");
+const largetempleLink = document.querySelector("#largetemple");
+const smalltempleLink = document.querySelector("#smalltemple");
+
+oldtempleLink.addEventListener("click", () => {
+    createTempleCard(temples.filter(temple => temple.dedicated.includes("")));
+});
+
+function createTempleCard(filteredTemples) {
+    document.querySelector(".figure-grid").innerHTML = "";
+    filteredTemples.forEach(temple => {
+        let card = document.createElement("section");
+        let name = document.createElement("h3");
+        let location = document.createElement("p");
+        let dedication = document.createElement("p");
+        let area = document.createElement("p");
+        let img = document.createElement("img");
+
+        name.textContent = temple.templeName;
+        location.innerHTML = `<span class="label">Location:</span> ${temple.location}`;
+        dedication.innerHTML = `<span class="label">Dedicated:</span> ${temple.dedicated}`;
+        area.innerHTML = `<span class="label">Size:</span> ${temple.area} sq ft`;
+        img.setAttribute("src", temple.imageUrl);
+        img.setAttribute("alt", `${temple.templeName} Temple`);
+        img.setAttribute("loading", "lazy");
+
+        card.appendChild(name);
+        card.appendChild(location);
+        card.appendChild(dedication);
+        card.appendChild(area);
+        card.appendChild(img);
+
+        document.querySelector(".figure-grid").appendChild(card);
+    });
+}
