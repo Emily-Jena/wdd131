64d1db64e6304b2f68c64e8ebf3081a3b53477b2
emily <emilyrpj11@gmail.com>
2024-07-22 23:52:33 +0200

    siteplan css

---
+++ project/script/project.js
@@ -0,0 +1,9 @@
+// -------------------------------------------------
+// Footer Dates - Get the current year
+const currentYear = new Date().getFullYear();
+
+// Output the copyright year
+const copyrightYear = document.getElementById('copyright-year');
+copyrightYear.textContent = currentYear;
+
+// --------------------------------------------------
