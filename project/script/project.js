206c315752f13a2e42f761b70124726d940dd14e
emily <emilyrpj11@gmail.com>
2024-07-24 12:08:09 +0200

    hamburger check

--- project/script/project.js
+++ project/script/project.js
@@ -7,3 +7,13 @@ const copyrightYear = document.getElementById('copyright-year');
 copyrightYear.textContent = currentYear;
 
 // --------------------------------------------------
+// Hamburger menu
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.nav-links');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
